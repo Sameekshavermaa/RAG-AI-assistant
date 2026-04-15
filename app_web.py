719a5823c1adee0775329aa6d50d229019751dca
Sameeksha <sameekshav69@gmail.com>
2026-04-15 17:03:14 +0530

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -85,7 +85,7 @@ if question_input:
     
     try:
         response = client.chat.completions.create(
-            model="mixtral-8x7b-32768",
+            model="llama-3.3-70b-versatile",
             messages=[{"role": "user", "content": prompt}]
         )
         answer = response.choices[0].message.content
